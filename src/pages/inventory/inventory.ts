97142f5c71516665fd91d40e16d2b0d2870d166a
aWiedebusch <awiedebusch@smu.edu>
2017-10-16 18:26:05 -0500

    Sprint1 framework

---
+++ src/pages/inventory/inventory.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-inventory',
+  templateUrl: 'inventory.html'
+})
+export class InventoryPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
