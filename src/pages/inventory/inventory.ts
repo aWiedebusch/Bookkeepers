712318ac69a3407e31f15bcd69f445d34b073a4e
aWiedebusch <awiedebusch@smu.edu>
2017-10-19 16:18:05 -0500

    inventory background almost done

--- src/pages/inventory/inventory.ts
+++ src/pages/inventory/inventory.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+import { BookPage } from '../book/book';
 
 @Component({
   selector: 'page-inventory',
@@ -7,8 +10,30 @@ import { NavController } from 'ionic-angular';
 })
 export class InventoryPage {
 
-  constructor(public navCtrl: NavController) {
+  all_books: any
 
+  constructor(public navCtrl: NavController, private storage: Storage) {
+      this.all_books = []
   }
+
+
+  ngOnInit() {
+    this.all_books = this.getBooks();
+  }
+
+  async getBooks() {
+    var index = await this.storage.keys()
+    var books = []
+
+    for(let book of index) {
+      books.push(this.storage.get(book))
+    }
+    return books
+  }
+
+  clickBook(isbn) {
+    this.navCtrl.push(BookPage,this.storage.get(isbn))
+  }
+
 
 }
