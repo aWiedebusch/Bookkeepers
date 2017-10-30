ed5c67a63ab3601f1baa723a28ccfca3259a8b0f
Adam Ashcraft <adamashcraft@Adams-MacBook-Pro.local>
2017-10-30 18:57:34 -0500

    Changed TypeScript to work with the rest of the app

--- src/pages/book/book.ts
+++ src/pages/book/book.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-book',
@@ -8,82 +8,43 @@ import { NavController } from 'ionic-angular';
 
 export class BookPage {
 
-  isbn: any;
-  title: any;
-  genre: any;
-  author: any;
-  price: any;
-  publisher: any;
-  condition: any;
-  additional_info: any;
+  book: any;
 
-  constructor(public navCtrl: NavController) {
-    this.isbn = null;
-    this.title = "Title text";
-    this.genre = "Genre text";
-    this.author = "Author text";
-    this.price = "Price text";
-    this.publisher = "Publisher text";
-    this.condition = "Condition text";
-    this.additional_info = "Description text";
-  }
-
-  getElem(element) {
-    
-    switch(element) {
-      case 'title':
-        return this.title;
-
-      case 'genre':
-        return this.genre;
-      
-      case 'author':
-        return this.author;
-
-      case 'price':
-        return this.price;
-      
-      case 'publisher':
-        return this.publisher;
-        
-      case 'condition':
-        return this.condition;
-
-      case 'desc':
-        return this.additional_info;
-    }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.book = navParams.get('Book');
   }
 
   editBook(element) {
 
     switch(element) {
       case 'title':
-        this.title = prompt('Please enter a new title');
+        this.book.title = prompt('Please enter a new title');
         break;
 
       case 'genre':
-        this.genre = prompt('Please enter a new genre');
+        this.book.genre = prompt('Please enter a new genre');
         break;
       
       case 'author':
-        this.author = prompt('Please enter a new author');
+        this.book.author = prompt('Please enter a new author');
         break;
 
       case 'price':
-        this.price = prompt('Please enter a new price');
+        this.book.price = prompt('Please enter a new price');
         break;
       
       case 'publisher':
-        this.publisher = prompt('Please enter a new publisher');
+        this.book.publisher = prompt('Please enter a new publisher');
         break;
         
       case 'condition':
-        this.condition = prompt('Please enter a new condition');
+        this.book.condition = prompt('Please enter a new condition');
         break;
 
       case 'desc':
-        this.additional_info = prompt('Please enter a new description');
+        this.book.additional_info = prompt('Please enter a new description');
         break;
     }
   }
 }
+
