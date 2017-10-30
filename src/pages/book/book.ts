57d498a59feed0657ca72f4b5e829f2afca7bea1
Adam Ashcraft <adamashcraft@Adams-MacBook-Pro.local>
2017-10-30 02:31:20 -0500

    Added TypeScript but can't figure out how to use it properly

--- src/pages/book/book.ts
+++ src/pages/book/book.ts
@@ -5,10 +5,85 @@ import { NavController } from 'ionic-angular';
   selector: 'page-book',
   templateUrl: 'book.html'
 })
+
 export class BookPage {
 
-  constructor(public navCtrl: NavController) {
+  isbn: any;
+  title: any;
+  genre: any;
+  author: any;
+  price: any;
+  publisher: any;
+  condition: any;
+  additional_info: any;
 
+  constructor(public navCtrl: NavController) {
+    this.isbn = null;
+    this.title = "Title text";
+    this.genre = "Genre text";
+    this.author = "Author text";
+    this.price = "Price text";
+    this.publisher = "Publisher text";
+    this.condition = "Condition text";
+    this.additional_info = "Description text";
   }
 
+  getElem(element) {
+    
+    switch(element) {
+      case 'title':
+        return this.title;
+
+      case 'genre':
+        return this.genre;
+      
+      case 'author':
+        return this.author;
+
+      case 'price':
+        return this.price;
+      
+      case 'publisher':
+        return this.publisher;
+        
+      case 'condition':
+        return this.condition;
+
+      case 'desc':
+        return this.additional_info;
+    }
+  }
+
+  editBook(element) {
+
+    switch(element) {
+      case 'title':
+        this.title = prompt('Please enter a new title');
+        break;
+
+      case 'genre':
+        this.genre = prompt('Please enter a new genre');
+        break;
+      
+      case 'author':
+        this.author = prompt('Please enter a new author');
+        break;
+
+      case 'price':
+        this.price = prompt('Please enter a new price');
+        break;
+      
+      case 'publisher':
+        this.publisher = prompt('Please enter a new publisher');
+        break;
+        
+      case 'condition':
+        this.condition = prompt('Please enter a new condition');
+        break;
+
+      case 'desc':
+        this.additional_info = prompt('Please enter a new description');
+        break;
+    }
+  }
 }
