92fd366f11de5b0e066741cfc40b2cb71995fdeb
Adam Ashcraft <adamashcraft@Adams-MacBook-Pro.local>
2017-10-30 19:35:34 -0500

    Editing book info works

--- src/pages/book/book.ts
+++ src/pages/book/book.ts
@@ -15,7 +15,35 @@ export class BookPage {
   }
 
   editBook(element) {
-    
+    switch(element) {
+      case 'title':
+        this.book.title = prompt('Please enter a new title');
+        break;
+
+      case 'genre':
+        this.book.genre = prompt('Please enter a new genre');
+        break;
+      
+      case 'author':
+        this.book.author = prompt('Please enter a new author');
+        break;
+
+      case 'price':
+        this.book.price = prompt('Please enter a new price');
+        break;
+      
+      case 'publisher':
+        this.book.publisher = prompt('Please enter a new publisher');
+        break;
+        
+      case 'condition':
+        this.book.condition = prompt('Please enter a new condition');
+        break;
+
+      case 'desc':
+        this.book.additional_info = prompt('Please enter a new description');
+        break;
+    }
   }
 }
 
