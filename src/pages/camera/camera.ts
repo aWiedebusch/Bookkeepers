ec590fca1886afd75a418bb62356e228a124981d
Adam Ashcraft <adamashcraft@Adams-MacBook-Pro.local>
2017-11-21 20:17:33 -0600

    Added a comment

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -28,6 +28,14 @@ export class CameraPage {
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
   }
+
+  // this.camera.getPicture(options).then((imageData) => {
+  //   // imageData is either a base64 encoded string or a file URI
+  //   // If it's base64:
+  //   let base64Image = 'data:image/jpeg;base64,' + imageData;
+  //  }, (err) => {
+  //   // Handle error
+  //  });
 }
 
 
